42fa20747e8b0c6ddfb66c51bc074c56fc8c1ab4
arturoromeroum <ajruwork@gmail.com>
2020-06-23 17:37:24 -0500

    se modifica la app

--- facilito_store/facilito_store/forms.py
+++ facilito_store/facilito_store/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
+from django.contrib.auth.models import User
+
 class RegisterForm(forms.Form):
+    
     username = forms.CharField(required=True,
                                 min_length=4,
                                 max_length=50,
@@ -8,14 +11,55 @@ class RegisterForm(forms.Form):
                                     'class': 'form-control',
                                     'id': 'username',
                                 }))
+    
     email = forms.EmailField(required=True,
                              widget=forms.EmailInput(attrs={
                                     'class': 'form-control',
                                     'id': 'email',
                                     'placeholder': 'example@email.com'
                                 }))
+    
     password = forms.CharField(required=True,
                                 widget=forms.PasswordInput(attrs={
                                     'class': 'form-control',
                                     'placeholder': 'password'
                                 }))
+
+    password2 = forms.CharField(label='Confirmar Password',
+                                required=True,
+                                widget=forms.PasswordInput(attrs={
+                                   'class': 'form-control',
+                                   'placeholder': 'password'
+                               }))
+
+    # con este metodo validamos el campo que queramos (usando clean_campo-a-validar)
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('El username ya esta siendo usado')
+        
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('El email ya esta registrado')
+        
+        return email
+
+    # esto solo se ultiliza si se quiere validar un campo y depende uno de otro.
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if cleaned_data.get('password2') != cleaned_data.get('password'):
+            self.add_error('password2', 'el password no coincide') 
+
+    def save(self):
+       return User.objects.create_user(
+           self.cleaned_data.get('username'),
+           self.cleaned_data.get('email'),
+           self.cleaned_data.get('password')
+        )
+        
--- facilito_store/facilito_store/views.py
+++ facilito_store/facilito_store/views.py
@@ -51,14 +51,15 @@ def register(request):
     form = RegisterForm(request.POST or None)
 
     # obtener informacion del formulario mediante una clase
-    if request.method == 'POST' and form.is_valid():
-        username = form.cleaned_data.get('username') # cleaned_data es un Diccionario
-        email = form.cleaned_data.get('email')
-        password = form.cleaned_data.get('password')
+    if request.method == 'POST' and form.is_valid(): # is_valid se encarga de validar cada campo
+        
 
-        print(username)
-        print(email)
-        print(password)
+        # esto nos va a retornar un objeto del tipo user (nos permite crear nuevos usuarios)
+        user = form.save()
+        if user:
+            login(request, user)
+            messages.success(request, 'creado exitosamente')
+            return redirect('index')
     
     return render(request, 'users/register.html', {
         'form': form

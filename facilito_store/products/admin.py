9d743fc8e21314be5878a36c84339ce25554281d
arturoromeroum <ajruwork@gmail.com>
2020-06-24 18:06:42 -0500

    se mejora la app y aparecen imagenes de los productos

--- facilito_store/products/admin.py
+++ facilito_store/products/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ('title', 'description', 'price', 'image')
+    list_display = ('__str__', 'slug', 'created_ad')
+
+admin.site.register(Product, ProductAdmin)
